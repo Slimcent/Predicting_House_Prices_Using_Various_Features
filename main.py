dc5b3ef3b1d5664272219b29f3b8fff620fd90de
Slimcent <centpresley@gmail.com>
2023-10-12 21:05:10 +0100

    Multiple regression

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Generate or load your dataset
+data = {
+    'SquareFootage': [1500, 2000, 1400, 1600, 2400, 1800, 2100, 1300],
+    'Bedrooms': [3, 4, 3, 3, 4, 3, 4, 2],
+    'Bathrooms': [2, 2.5, 2, 2, 3, 2, 3, 1.5],
+    'DistanceToSchool': [0.5, 1.2, 0.8, 0.3, 1.5, 0.7, 1.0, 0.4],
+    'AgeOfHouse': [5, 10, 8, 2, 15, 7, 12, 3],
+    'Price': [250000, 320000, 270000, 260000, 400000, 290000, 350000, 230000]
+}
 
+df = pd.DataFrame(data)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Split the dataset into training and testing sets
+X = df.drop('Price', axis=1)
+y = df['Price']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Create and fit the multiple linear regression model (same as before)
+model = LinearRegression()
+model.fit(X_train, y_train)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Make predictions on the test set (same as before)
+y_pred = model.predict(X_test)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Plot the regression line along with actual data points
+plt.scatter(X_test['SquareFootage'], y_test, color='blue', label='Actual Prices')
+plt.plot(X_test['SquareFootage'], y_pred, color='red', linewidth=2, label='Regression Line')
+plt.xlabel('Square Footage')
+plt.ylabel('Price')
+plt.title('Multiple Linear Regression')
+plt.legend()
+plt.show()
